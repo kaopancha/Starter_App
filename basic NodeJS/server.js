609087a9095acad70776ca89fd65ac444cf26f35
Praphan <praphan.pae@gmail.com>
2021-06-11 09:36:30 +0700

    จบ HTML page + Routing

--- basic NodeJS/server.js
+++ basic NodeJS/server.js
@@ -1,5 +1,33 @@
 var http=require('http')
+var fs=require('fs')
+var myUser={
+    "name":"Kaopancha",
+    "job":"Programmer",
+    "age":50
+}
 http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/plain'})
-    res.end("Helloworld")
+
+    // jsonData
+    // res.writeHead(200,{'Content-Type':'application/json'})
+    // res.end(JSON.stringify(myUser))
+
+    if(req.url ==='/home' || req.url==='/'){
+        res.writeHead(200,{'Content-Type':'text/html'})
+        var myStream=fs.createReadStream(__dirname+"/"+"index.html",'utf8')
+        myStream.pipe(res)
+    }
+    else if(req.url ==='/facebook'){
+        res.writeHead(200,{'Content-Type':'text/html'})
+        var myStream=fs.createReadStream(__dirname+"/"+"facebook.html",'utf8')
+        myStream.pipe(res)
+    }
+    else if(req.url ==='/youtube'){
+        res.writeHead(200,{'Content-Type':'text/html'})
+        var myStream=fs.createReadStream(__dirname+"/"+"youtube.html",'utf8')
+        myStream.pipe(res)
+    }else{
+        res.writeHead(404,{'Content-Type':'text/html'})
+        var myStream=fs.createReadStream(__dirname+"/"+"notfound.html",'utf8')
+        myStream.pipe(res)
+    }
 }).listen(8081,'127.0.0.1')
