43e991aadfb6667e1f32c64808372e7dbf94db82
Praphan <praphan.pae@gmail.com>
2021-06-19 10:39:22 +0700

    routing & middleware

--- basic NodeJS/express/app.js
+++ basic NodeJS/express/app.js
@@ -3,7 +3,15 @@ var routing=express()
 routing.get('/',function(req,res){
     res.send("<h1>Hello World</h1>")
 })
-routing.get('/profile/:name',function(req,res){
+
+routing.use('/profile/:name',function(req,res,next){
+    // middleware ดักจับ ตรวจสอบ request (แสดงรูปแบบ log)
+    console.log("Request:"+ req.params.name + new Date() , req.method,req.url)
+
     res.send("<h1>Created by "+ req.params.name +"</h1>")
 })
+
+// routing.get('/profile/:name',function(req,res){
+//     res.send("<h1>Created by "+ req.params.name +"</h1>")
+// })
 routing.listen(8081)
