fcae5e75399b5fe9ade17c76a9209e71acd2e563
Praphan <praphan.pae@gmail.com>
2021-06-22 08:50:35 +0700

    จบ Post (body-parser)

--- basic NodeJS/express/appMethod.js
+++ basic NodeJS/express/appMethod.js
@@ -1,16 +1,21 @@
+// Express
 var express = require('express')
 var app = express()
+// Body-parser
+var body = require('body-parser')
+app.use(body())
+
 app.set('view engine','ejs')
 app.get('/showForm',function(req,res){
     res.sendFile(__dirname+"/"+"showForm.html")
 })
-app.get('/showData',function(req,res){
+app.post('/showData',function(req,res){
     data={
-        fname:req.query.fname,
-        lname:req.query.lname
+        fname:req.body.fname,
+        lname:req.body.lname
     }
     console.log(data)
-    res.end(JSON.stringify(data))
+    res.end("<h1> Firstname :"+data.fname+"</h1>")
 })
 
 app.listen(8081)
