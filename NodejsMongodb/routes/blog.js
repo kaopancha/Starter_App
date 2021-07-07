3819b6409b407adb27c3783c03443fc9065dc4d4
Praphan <praphan.pae@gmail.com>
2021-07-07 09:19:48 +0700

    check request (errors)

--- NodejsMongodb/routes/blog.js
+++ NodejsMongodb/routes/blog.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var router = express.Router();
+/* express validator (check request)*/ 
+const { body, validationResult } = require('express-validator');
 
 /* GET all products. */
 router.get('/', function(req, res, next) {
     res.render('blog');
 });
+
 router.get('/add', function(req, res, next) {
     res.render('addBlog');
+});
+
+router.post(
+    '/add',
+    body("articleName","กรุณาระบุชื่อบทความ").not().isEmpty(),
+    body("articleDesc","กรุณาระบุรายละเอียดบทความ").not().isEmpty(),
+    body("writerName","กรุณาระบุชื่อผู้เขียน").not().isEmpty(),
+    function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+    }
+    else{
+        // insert to db
+    }
 });
 
 module.exports = router;
